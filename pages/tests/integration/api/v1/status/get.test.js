fae00a4ca1e0a50cc59d2649089f0af46784c4da
Ronaldo Silva <49988756+ronaldointermec@users.noreply.github.com>
2026-02-09 02:31:20 +0000

    bugfig `get.test.js`

--- pages/tests/integration/api/v1/status/get.test.js
+++ pages/tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,3 @@
-const { RESPONSE_LIMIT_DEFAULT } = require("next/dist/server/api-utils");
-
 test("Get to /api/v1/status shour returno 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
