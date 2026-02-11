db5a7a61ddfa1a55557c0a85f2ceb0b76782f02c
Ronaldo Silva <49988756+ronaldointermec@users.noreply.github.com>
2026-02-11 02:51:07 +0000

    addd `version` `max_connection`` used_connection`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,25 @@
 import database from "infra/database.js";
 
+/**
+ * 1) Versão do Postgres
+ * 2) Conexões máximas
+ * 3) Conexões usadas
+ */
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS SUM;");
-  console.log("1 + 1 eh: ", result.rows);
-  response.status(200).json({ chave: "valor" });
+  const query = `
+  SELECT 
+    current_setting('server_version') AS "version",
+    current_setting('max_connections')::int AS "max",
+    (SELECT count(*) FROM pg_stat_activity)::int AS "used";
+`;
+  const result = await database.query(query);
+  const updatedAt = new Date().toISOString();
+  response.status(200).json({
+    updated_at: updatedAt,
+    version: result.rows[0].version,
+    max_connection: result.rows[0].max,
+    used_connection: result.rows[0].used,
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,23 @@
 test("GET to /api/v1/status shourd return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  console.log(responseBody);
+  expect(responseBody.updated_at).toBeDefined();
+  const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parseUpdatedAt);
+
+  expect(responseBody.version).toBeDefined();
+  expect(typeof responseBody.version).toBe("string");
+
+  // 2. Valida se as conexões são números e fazem sentido matemático
+  expect(responseBody.max_connection).toBeDefined();
+  expect(responseBody.max_connection).toBeGreaterThan(0);
+
+  expect(responseBody.used_connection).toBeDefined();
+  expect(responseBody.used_connection).toBeGreaterThan(0);
+  expect(responseBody.used_connection).toBeLessThanOrEqual(
+    responseBody.max_connection,
+  );
 });
