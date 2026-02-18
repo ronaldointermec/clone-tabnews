f4c871a5aeec389527e7292b9e4a2052062a8b29
Ronaldo Silva <49988756+ronaldointermec@users.noreply.github.com>
2026-02-18 12:53:11 +0000

    adds migrations script

--- infra/database.js
+++ infra/database.js
@@ -6,14 +6,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: true,
-  });
-  console.log("Credencias do Postgres:", {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
 
   try {
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,24 +3,10 @@ test("GET to /api/v1/status shourd return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  //expect(responseBody.updated_at).toBeDefined();
   const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parseUpdatedAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.11 (df20cf9)");
-  expect(responseBody.dependencies.database.max_connections).toEqual(901);
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.used_connections).toEqual(1);
-
-  // expect(responseBody.version).toBeDefined();
-  // expect(typeof responseBody.version).toBe("string");
-
-  // // 2. Valida se as conexões são números e fazem sentido matemático
-  // expect(responseBody.max_connection).toBeDefined();
-  // expect(responseBody.max_connection).toBeGreaterThan(0);
-
-  // expect(responseBody.used_connection).toBeDefined();
-  // expect(responseBody.used_connection).toBeGreaterThan(0);
-  // expect(responseBody.used_connection).toBeLessThanOrEqual(
-  //   responseBody.max_connection,
-  // );
 });
