a28592b3ccc83789fd7da21c680107765d4fe0e8
Ronaldo Silva <49988756+ronaldointermec@users.noreply.github.com>
2026-03-21 19:02:52 +0000

    ci: add `commitlint` with `Conventional Commits` config

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -7,7 +7,7 @@ beforeAll(async () => {
   database.query("DROP SCHEMA PUBLIC CASCADE; CREATE SCHEMA PUBLIC;");
 });
 
-test("GET to /api/v1/migrations shourd return 200", async () => {
+test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
 
