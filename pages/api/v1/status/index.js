4bcb7805cfbd81a2642ad3dd539e63b82abc9516
Ronaldo Silva <49988756+ronaldointermec@users.noreply.github.com>
2026-05-04 12:34:50 +0000

    fix: ajusta um bug de encerramento de conexão no `database.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,48 +1,58 @@
 import database from "infra/database.js";
-
+import { InternalServerError } from "infra/errors";
 async function status(request, response) {
-  //   const query = `
-  //   SELECT
-  //     current_setting('server_version') AS "version",
-  //     current_setting('max_connections')::int AS "max",
-  //     (SELECT count(*) FROM pg_stat_activity)::int AS "used";
-  // `;
-  // const result = await database.query(query);
-  const databaseVersionResult = await database.query("SHOW server_version;");
-  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  try {
+    //   const query = `
+    //   SELECT
+    //     current_setting('server_version') AS "version",
+    //     current_setting('max_connections')::int AS "max",
+    //     (SELECT count(*) FROM pg_stat_activity)::int AS "used";
+    // `;
+    // const result = await database.query(query);
+    const databaseVersionResult = await database.query("SHOW server_version;");
+    const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  const databaseMaxConnectionResult = await database.query(
-    "SHOW max_connections;",
-  );
-  const databaseMaxConnectionValue =
-    databaseMaxConnectionResult.rows[0].max_connections;
+    const databaseMaxConnectionResult = await database.query(
+      "SHOW max_connections;",
+    );
+    const databaseMaxConnectionValue =
+      databaseMaxConnectionResult.rows[0].max_connections;
 
-  const databaseName = process.env.POSTGRES_DB;
+    const databaseName = process.env.POSTGRES_DB;
 
-  const databaseOpendConnectionsResult = await database.query({
-    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname =  $1;",
-    values: [databaseName],
-  });
+    const databaseOpendConnectionsResult = await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname =  $1;",
+      values: [databaseName],
+    });
 
-  const databaseOpendConnectionsValue =
-    databaseOpendConnectionsResult.rows[0].count;
+    const databaseOpendConnectionsValue =
+      databaseOpendConnectionsResult.rows[0].count;
 
-  console.log(databaseOpendConnectionsValue);
+    console.log(databaseOpendConnectionsValue);
 
-  const updatedAt = new Date().toISOString();
-  response.status(200).json({
-    updated_at: updatedAt,
-    dependencies: {
-      database: {
-        version: databaseVersionValue,
-        max_connections: parseInt(databaseMaxConnectionValue),
-        used_connections: databaseOpendConnectionsValue,
+    const updatedAt = new Date().toISOString();
+    response.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          version: databaseVersionValue,
+          max_connections: parseInt(databaseMaxConnectionValue),
+          used_connections: databaseOpendConnectionsValue,
+        },
       },
-    },
-    // version: result.rows[0].version,
-    // max_connection: result.rows[0].max,
-    // used_connection: result.rows[0].used,
-  });
+      // version: result.rows[0].version,
+      // max_connection: result.rows[0].max,
+      // used_connection: result.rows[0].used,
+    });
+  } catch (error) {
+    const publicErrorObject = new InternalServerError({
+      cause: error,
+    });
+
+    console.log("\n Erro dentro do catch do controller: ");
+    console.error(publicErrorObject);
+    response.status(500).json(publicErrorObject);
+  }
 }
 
 export default status;
