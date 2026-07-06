19dde92ad877a69de3c9d875497b173edbf513eb
root <root@HON-GPSDKT3.>
2026-07-06 16:15:43 -0300

    feat(cookie): adiciona cookie sessão

---
+++ pages/api/v1/sessions/index.js
@@ -0,0 +1,34 @@
+import { createRouter } from "next-connect";
+import * as cookie from "cookie";
+import controller from "infra/controller.js";
+import authentication from "models/authentication.js";
+import session from "models/session.js";
+
+const router = createRouter();
+router.post(postHandler);
+
+export default router.handler(controller.errorHandlers);
+
+async function postHandler(request, response) {
+  const userInputValues = request.body;
+
+  const authenticatedUser = await authentication.getAuthenticatedUser(
+    userInputValues.email,
+    userInputValues.password,
+  );
+
+  const newSession = await session.create(authenticatedUser.id);
+
+  const setCooke = cookie.serialize("session_id", newSession.token, {
+    path: "/",
+    // expires: new Date(newSession.expires_at),
+    maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000, // Convert milliseconds to seconds
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    // sameSite: "strict",
+  });
+
+  response.setHeader("Set-Cookie", setCooke);
+
+  return response.status(201).json(newSession);
+}
