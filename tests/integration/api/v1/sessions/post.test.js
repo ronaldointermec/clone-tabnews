563ef00766dbe5d82e20545d249c0b4cc3ee7eb7
root <root@HON-GPSDKT3.>
2026-07-06 16:18:37 -0300

    feat: cria teste de sessão

--- tests/integration/api/v1/sessions/post.test.js
+++ tests/integration/api/v1/sessions/post.test.js
@@ -136,7 +136,6 @@ describe("POST /api/v1/sessions", () => {
         map: true,
       });
 
-      console.log("parsedSetCookie:", parsedSetCookie);
       expect(parsedSetCookie.session_id).toEqual({
         name: "session_id",
         value: responseBody.token,
