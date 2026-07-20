608635a12a65c84ee640d68d7334b1c99797e3a1
root <root@HON-GPSDKT3.>
2026-07-20 14:39:58 -0300

    chore: remove commented code

--- pages/api/v1/sessions/index.js
+++ pages/api/v1/sessions/index.js
@@ -19,17 +19,6 @@ async function postHandler(request, response) {
 
   const newSession = await session.create(authenticatedUser.id);
 
-  // const setCooke = cookie.serialize("session_id", newSession.token, {
-  //   path: "/",
-  //   // expires: new Date(newSession.expires_at),
-  //   maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000, // Convert milliseconds to seconds
-  //   httpOnly: true,
-  //   secure: process.env.NODE_ENV === "production",
-  //   // sameSite: "strict",
-  // });
-
-  // response.setHeader("Set-Cookie", setCooke);
-
   controller.setSessionCookie(newSession.token, response)
 
   return response.status(201).json(newSession);
