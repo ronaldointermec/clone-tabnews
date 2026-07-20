866a4c0972e0478824d0b858c9703a05966eed3c
root <root@HON-GPSDKT3.>
2026-07-20 15:32:04 -0300

    fix: adiciona `Cache-Control`

--- pages/api/v1/user/index.js
+++ pages/api/v1/user/index.js
@@ -17,5 +17,10 @@ async function getHandler(request, response) {
   controller.setSessionCookie(sessionObject.token, response);
   const userFound = await user.findOneById(renewdSessionObject.user_id);
 
+  response.setHeader(
+    "Cache-Control",
+    "no-store, no-cache, max-age=0, must-revalidate",
+  );
+
   return response.status(200).json(userFound);
 }
